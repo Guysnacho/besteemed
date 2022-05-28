34db812d24f34ca157106b80f9797e52be3ebe07
Guysnacho <guysnacho2@gmail.com>
2022-05-28 02:07:51 -0500

    Add placeholder pages

---
+++ pages/bookstore.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Bookstore: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Bookstore - Bosede Adetunji</title>
+        <meta
+          name="description"
+          content="Welcome to the home of an Esteemed Woman!"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Bookstore;
---
+++ pages/services.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Services: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Services - Bosede Adetunji</title>
+        <meta
+          name="description"
+          content="Welcome to the home of an Esteemed Woman!"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Services;
