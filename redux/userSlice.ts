98a5d50a2068ae3217a72bc8537e8ba84dbfaf0a
Guysnacho <guysnacho2@gmail.com>
2022-09-05 18:51:24 -0500

    Add rtk and supabase

---
+++ redux/userSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { getAuth } from "firebase/auth";
+import { fbase } from "../pages/api/Firebase";
+import { RootState } from "./store";
+
+export interface userState {
+  uid?: string | null | undefined;
+  displayName?: string | null | undefined;
+  email?: string | null | undefined;
+  phoneNumber?: string | null | undefined;
+  photoURL?: string | null | undefined;
+}
+
+//Setup database interface
+export interface dbReqs {}
+
+var initialState = {} as userState;
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<userState>) => {
+      //Set state
+      state = action.payload;
+    },
+    logout: (state) => {
+      state = {
+        uid: "",
+        displayName: "",
+        email: "",
+        phoneNumber: "",
+        photoURL: "",
+      };
+      const auth = getAuth(fbase);
+      auth.signOut();
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectUser = (state: RootState) => state.user;
+
+export default userSlice.reducer;
