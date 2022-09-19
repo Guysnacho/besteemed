8916c6cb65909228c5594b6df2020cc44db76bb8
Guysnacho <guysnacho2@gmail.com>
2022-09-19 02:41:03 -0500

    Add form with database bindings

--- Components/Individual/InterestForm.tsx
+++ Components/Individual/InterestForm.tsx
@@ -1,13 +1,22 @@
 import {
   Alert,
+  Box,
   Button,
   Card,
   CardContent,
   CardMedia,
+  Chip,
   FormControl,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
   Stack,
   TextField,
-  Typography
+  Theme,
+  Typography,
+  useTheme
 } from "@mui/material";
 import Backdrop from "@mui/material/Backdrop";
 import Fade from "@mui/material/Fade";
@@ -19,6 +28,25 @@ import { supabase } from "../../utils/supabaseClient";
 
 const emailVal = new RegExp("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$");
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+const interestList = [
+  "Book Releases",
+  "Esteemed Woman",
+  "Excursions",
+  "Speaker Events",
+  "Leadership Training",
+  "CPR",
+];
+
 const InterestForm = (props: {
   open: boolean;
   handleOpen: () => void;
@@ -29,32 +57,70 @@ const InterestForm = (props: {
   const [email, setEmail] = useState("");
   const [fname, setFname] = useState("");
   const [lname, setLname] = useState("");
+  const [selection, setSelection] = useState<string[]>([]);
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
-  // Component function
-  const handleSubmit = () => {
+  const theme = useTheme();
+
+  // Component functions
+  const handleSignUp = () => {
+    setErrorMessage("");
+    setSuccessMessage("");
     setLoading(true);
-    if (email) {
-      supabase.auth.api.resetPasswordForEmail(email).then((res) => {
-        setSuccessMessage("Check your email for a link :)");
-        if (res.data) {
-          setSuccessMessage("Check your email for a confirmation");
-        } else if (res.error) {
-          console.error("You got an error - " + res.error.message);
-          setErrorMessage(res.error.message);
+
+    supabase
+      .from("profiles")
+      .upsert(
+        {
+          fname: fname,
+          lname: lname,
+          interests: selection
+            ? [...selection].sort((a, b) => a.localeCompare(b))
+            : undefined,
+          email: email,
+        },
+        { onConflict: "email", returning: "minimal", ignoreDuplicates: false }
+      )
+      .then((res) => {
+        if (res.error) {
+          setErrorMessage("Something went wrong - " + res.error.message);
         } else {
-          setErrorMessage(
-            "Something went wrong along the way, try again later"
+          setSuccessMessage(
+            "Thank you for your interest! We'd love to keep in touch"
           );
         }
       });
-    } else {
-      setErrorMessage("You didn't fill everything in");
-    }
+
     setEmail("");
+    setFname("");
+    setLname("");
+    setSelection([]);
     setLoading(false);
   };
+
+  const handleChange = (event: SelectChangeEvent<typeof selection>) => {
+    const {
+      target: { value },
+    } = event;
+    setSelection(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
+  function getStyles(
+    interest: string,
+    selection: readonly string[],
+    theme: Theme
+  ) {
+    return {
+      fontWeight:
+        selection.indexOf(interest) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+    };
+  }
 
   return (
     <div>
@@ -70,97 +136,156 @@ const InterestForm = (props: {
         }}
       >
         <Fade in={props.open}>
-          {successMessage !== "" ? (
-            <Alert severity="success">
-              {"Please check your email for a confirmation"}
-            </Alert>
-          ) : (
-            <Card
+          <Card
+            sx={{
+              position: "absolute" as "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: { xs: "75vw", sm: "50vw", md: "40vw" },
+              height: { xs: "75vh", sm: "80vh", md: "80vh" },
+              borderRadius: "25px 0px 25px 0px",
+            }}
+            elevation={15}
+          >
+            <CardMedia>
+              <Image src={banner} alt="Stylish banner" />
+            </CardMedia>
+            <CardContent
               sx={{
-                position: "absolute" as "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                width: { xs: "65vw", sm: "50vw", md: "40vw" },
-                borderRadius: "25px 0px 25px 0px",
+                textAlign: "center",
+                py: { xs: 2, md: 1 },
+                width: { xs: "75vw", sm: "50vw", md: "40vw" },
+                maxHeight: { xs: "60vh", sm: "75vh", md: "80vh" },
+                overflowY: "auto",
               }}
-              elevation={15}
             >
-              <CardMedia>
-                <Image src={banner} alt="Stylish banner" />
-              </CardMedia>
-              <CardContent
-                sx={{
-                  textAlign: "center",
-                  py: { xs: 2, md: 1 },
-                  px: { xs: 4, md: 1 },
-                }}
-              >
-                <Typography variant="h6">Keep in Touch</Typography>
-                <FormControl sx={{ mt: 2 }}>
-                  <Stack spacing={4} direction="column" my="auto">
-                    <TextField
-                      id="outlined-first-name-input"
-                      variant="outlined"
-                      label="First Name"
-                      type="text"
-                      aria-label="first name"
-                      placeholder="Ngozi"
-                      defaultValue="Ngozi"
-                      error={errorMessage !== ""}
-                      autoComplete="first-name"
-                      value={fname}
-                      onChange={(e) => {
-                        setFname(e.target.value);
-                      }}
-                      disabled={loading}
-                      required
-                    />
-                    <TextField
-                      id="outlined-family-name-input"
-                      variant="outlined"
-                      label="Last Name"
-                      type="text"
-                      aria-label="family name"
-                      placeholder="Okonjo-Iweala"
-                      defaultValue="Okonjo-Iweala"
-                      error={errorMessage !== ""}
-                      autoComplete="family-name"
-                      value={lname}
-                      onChange={(e) => {
-                        setLname(e.target.value);
-                      }}
-                      disabled={loading}
-                      required
-                    />
-                    <TextField
-                      id="outlined-username-input"
-                      variant="outlined"
-                      label="Email"
-                      type="email"
-                      error={!emailVal.test(email)}
-                      helperText={errorMessage}
-                      value={email}
-                      onChange={(e) => {
-                        setEmail(e.target.value);
-                      }}
-                      disabled={loading}
-                      required
-                    />
-                    <Button
-                      variant="outlined"
-                      aria-label="login"
-                      onClick={handleSubmit}
-                      sx={{ mb: 2 }}
-                      disabled={!(fname && lname && emailVal.test(email))}
-                    >
-                      Join the Community 🤗
-                    </Button>
-                  </Stack>
-                </FormControl>
-              </CardContent>
-            </Card>
-          )}
+              {errorMessage !== "" ? (
+                <Alert severity="error" sx={{ m: "auto" }}>
+                  {"Oops, something went wrong - " + errorMessage}
+                </Alert>
+              ) : (
+                <></>
+              )}
+              {successMessage !== "" ? (
+                <Alert severity="success" sx={{ m: "auto" }}>
+                  {successMessage}
+                </Alert>
+              ) : (
+                <Box sx={{ mb: { xs: 4, sm: 16, md: 15 } }}>
+                  <Typography variant="h6">Keep in Touch</Typography>
+                  <FormControl sx={{ mt: 2 }}>
+                    <Stack spacing={4} direction="column">
+                      <TextField
+                        id="outlined-first-name-input"
+                        variant="outlined"
+                        label="First Name"
+                        type="text"
+                        aria-label="first name"
+                        placeholder="Ngozi"
+                        error={errorMessage !== ""}
+                        autoComplete="first-name"
+                        value={fname}
+                        onChange={(e) => {
+                          setFname(e.target.value);
+                        }}
+                        disabled={loading}
+                        required
+                      />
+                      <TextField
+                        id="outlined-family-name-input"
+                        variant="outlined"
+                        label="Last Name"
+                        type="text"
+                        aria-label="family name"
+                        placeholder="Okonjo-Iweala"
+                        error={errorMessage !== ""}
+                        autoComplete="family-name"
+                        value={lname}
+                        onChange={(e) => {
+                          setLname(e.target.value);
+                        }}
+                        disabled={loading}
+                        required
+                      />
+                      <TextField
+                        id="outlined-username-input"
+                        variant="outlined"
+                        label="Email"
+                        type="email"
+                        placeholder="esteemed@woman.com"
+                        error={!emailVal.test(email)}
+                        value={email}
+                        onChange={(e) => {
+                          setEmail(e.target.value);
+                        }}
+                        disabled={loading}
+                        required
+                      />
+                      <div>
+                        <FormControl sx={{ width: "100%" }}>
+                          <InputLabel id="demo-multiple-chip-label">
+                            Interests
+                          </InputLabel>
+                          <Select
+                            labelId="multiple-chip-label"
+                            id="multiple-chip"
+                            multiple
+                            value={selection}
+                            onChange={handleChange}
+                            input={
+                              <OutlinedInput
+                                id="select-multiple-chip"
+                                label="Interest"
+                              />
+                            }
+                            renderValue={(selected) => (
+                              <Box
+                                sx={{
+                                  display: "flex",
+                                  flexWrap: "wrap",
+                                  gap: 0.5,
+                                }}
+                              >
+                                {selected.map((value) => (
+                                  <Chip key={value} label={value} />
+                                ))}
+                              </Box>
+                            )}
+                            MenuProps={MenuProps}
+                          >
+                            {interestList.map((currentInterest) => (
+                              <MenuItem
+                                key={currentInterest}
+                                value={currentInterest}
+                                style={getStyles(
+                                  currentInterest,
+                                  selection,
+                                  theme
+                                )}
+                              >
+                                {currentInterest}
+                              </MenuItem>
+                            ))}
+                          </Select>
+                        </FormControl>
+                      </div>
+                      <Button
+                        variant="outlined"
+                        aria-label="login"
+                        onClick={handleSignUp}
+                        disabled={
+                          !(fname && lname && emailVal.test(email)) || loading
+                        }
+                      >
+                        Join the Community 🤗
+                      </Button>
+                    </Stack>
+                  </FormControl>
+                </Box>
+              )}
+            </CardContent>
+          </Card>
         </Fade>
       </Modal>
     </div>
